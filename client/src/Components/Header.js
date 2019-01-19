5a718ca84288edb34cc73b9577d850360dc324fe
unknown <robt9095@gmail.com>
2019-01-19 11:40:20 -0800

    updates to header to store navigation menu items in state

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom'; 
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import Sidebar from './Sidebar';
 
 export default class Header extends Component {
   state = {
     showSidebar: false,
+    menuItems: ['About','Projects','Contact'],
   }
   toggleSidebar = () => {
     this.setState({
@@ -21,15 +21,14 @@ export default class Header extends Component {
               <a href="/"><h3>Rob Tucker</h3></a>
             </div>
             <nav name="Desktop Navigation" className="desktop-nav">
-              <li>
-                {this.props.useAnchorLinks ? <AnchorLink href="#about">About</AnchorLink> : <Link to="/#about">About</Link> }
-              </li>
-              <li>
-                {this.props.useAnchorLinks ? <AnchorLink href="#projects">Projects</AnchorLink> : <Link to="/#projects">Projects</Link>}
-              </li>
-              <li>
-                {this.props.useAnchorLinks ? <AnchorLink href="#contact">Contact</AnchorLink> : <Link to="/#contact">Contact</Link>}
-              </li>
+              {this.state.menuItems.map(item=>(
+                <li key={item}>
+                  {this.props.useAnchorLinks ?
+                    <AnchorLink href="#about">About</AnchorLink>
+                    :
+                    <a href={"/#"+item.toLowerCase()}>{item}</a> }
+                </li>
+              ))}
             </nav>
             <nav name="Mobile Navigation" className="mobile-nav">
               <button className="btn-link" title="Open Navigation">
